9a9d9bed1f9f317e237dd84a5834a463da88639d
Mareliusmoen <mareliusmoen@gmail.com>
2023-12-15 13:24:27 +0100

    Fix the shoppinglist serializer to allow items as JSON array

--- todo_project/todo_api/serializers.py
+++ todo_project/todo_api/serializers.py
@@ -7,28 +7,24 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'email', 'is_active', 'is_admin']
 
-
 class ShoppingListSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShoppingList
         fields = ['id', 'name', 'items']
 
-    def to_representation(self, instance):
-        ret = super().to_representation(instance)
-        return ret
-
-    def to_internal_value(self, data):
-        internal_value = super().to_internal_value(data)
-        items_list = data.get('items', [])
-        internal_value['items'] = items_list
-        return internal_value
-
     def create(self, validated_data):
+        items_data = validated_data.pop('items', [])
         shopping_list = ShoppingList.objects.create(**validated_data)
+
+        # Assuming items_data is a list of dictionaries containing item details
+        shopping_list.items = items_data
+        shopping_list.save()
+
         return shopping_list
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
         instance.items = validated_data.get('items', instance.items)
         instance.save()
+
         return instance
