4d67beb50c5ffcc00f169c339568b034e90b37f1
Mareliusmoen <mareliusmoen@gmail.com>
2023-12-18 15:34:46 +0100

    Connect the App.jsx to the Vite_api_url

--- todo_project/frontend/chorify-frontend/src/App.jsx
+++ todo_project/frontend/chorify-frontend/src/App.jsx
@@ -1,32 +1,62 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-
-  const [data, setData] = useState([])
+  const [shoppingLists, setShoppingLists] = useState([]);
+  const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    async function fetchData() {
+    async function fetchShoppingLists() {
       try {
-        const response = await fetch(`${import.meta.env.VITE_API_URL}`);
+        const response = await fetch(`${import.meta.env.VITE_API_URL}shopping-lists/`);
         if (response.ok) {
           const result = await response.json();
-          setData(result);
-          console.log(result);
+          setShoppingLists(result);
         } else {
-          throw new Error('Network response was not ok.');
+          throw new Error('Network response was not ok for shopping lists.');
         }
       } catch (error) {
-        console.error('There has been a problem with your fetch operation:', error);
+        console.error('There has been a problem with your fetch operation for shopping lists:', error);
       }
     }
-    fetchData();
+
+    // async function fetchTodos() {
+    //   try {
+    //     const response = await fetch(`${import.meta.env.VITE_API_URL}todos/`);
+    //     if (response.ok) {
+    //       const result = await response.json();
+    //       setTodos(result);
+    //     } else {
+    //       throw new Error('Network response was not ok for todos.');
+    //     }
+    //   } catch (error) {
+    //     console.error('There has been a problem with your fetch operation for todos:', error);
+    //   }
+    // }
+
+    fetchShoppingLists();
+    // fetchTodos();
   }, []);
+
   return (
     <>
-    Hello world
+      <h1>Shopping Lists</h1>
+      {/* Render shopping lists here */}
+      <ul>
+        {shoppingLists.map(list => (
+          <li key={list.id}>{list.name}</li>
+        ))}
+      </ul>
+
+      <h1>To-dos</h1>
+      {/* Render todos here */}
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
