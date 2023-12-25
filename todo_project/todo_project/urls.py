8c9a6c8759344e8ebef7a3ac4260b3480ac171db
Mareliusmoen <mareliusmoen@gmail.com>
2023-12-25 12:38:18 +0100

    Setup TemplateView in the urls.py file

--- todo_project/todo_project/urls.py
+++ todo_project/todo_project/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('todo_api.urls')),
+    path('api/', include('todo_api.urls')),
     path('api/auth/', include('dj_rest_auth.urls')),
     path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
+
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
