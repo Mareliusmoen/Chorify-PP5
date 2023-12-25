e57bd9257f4ea7912b3957389be4518d2cbb08aa
Mareliusmoen <mareliusmoen@gmail.com>
2023-12-25 14:49:25 +0100

    wsgi setup

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -179,7 +179,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'dist'),
 ]
