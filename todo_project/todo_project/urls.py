9fd2b8eb2e7c1e066fb729b98ba522b35db60605
Mareliusmoen <mareliusmoen@gmail.com>
2023-12-25 14:45:32 +0100

    set urls.py to try and fix MIME error

--- todo_project/todo_project/urls.py
+++ todo_project/todo_project/urls.py
@@ -10,8 +10,3 @@ urlpatterns = [
     path('auth/', include('dj_rest_auth.urls')),
     path('auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
